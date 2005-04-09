89adbbfe964420506d34ed92f8b65b467626f885
gam3 <gam3@ebf68bd0-76f2-0310-8cb3-f3ea0bc37016>
2005-04-09 03:33:34 +0000

    The t1 clock was not working.  This kept the cursor from blinking.

--- trunk/src/actions.c
+++ trunk/src/actions.c
@@ -633,6 +633,7 @@ do_shutdown()
         saturn.timer2 = ticks.t2_ticks;
       }
       saturn.timer1 = set_t1 - ticks.t1_ticks;
+      set_t1 = ticks.t1_ticks;
 
       interrupt_called = 0;
       if (GetEvent()) {
